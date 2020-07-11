c0db8a6c82016c11c84a9ab27370d28349d51a57
Lucas Vanni <lucas.vanni@hotmail.com>
2020-07-11 02:06:43 -0300

    Fix in collors

--- src/api/functionsAPI.js
+++ src/api/functionsAPI.js
@@ -100,8 +100,7 @@ export const checkCollectPoints = () => {
             buttonbackGroundColor: items.val().buttonbackGroundColor,
           });
         });
-
-        resolve(checkCollectList);
       });
+    resolve(checkCollectList);
   });
 };
